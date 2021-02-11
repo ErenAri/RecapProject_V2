642a1232b88b15858c36f6c3f7cd3ff96c86787f
ErenAri <67584761+ErenAri@users.noreply.github.com>
2021-02-11 17:14:03 +0300

    küçük bir güncelleme

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,6 +6,7 @@ namespace Business.Constants
 {
     public static class Messages
     {
+        public static string MaintenanceTime = "Sistem şuan bakımdadır";
         public static string CustomerAdded = "Müşteri eklendi";
         public static string BrandAdded = "Marka eklendi";
         public static string ColorAdded = "Renk eklendi";
